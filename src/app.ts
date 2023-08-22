698e07f344aa1639d0dad196d557506049872dc1
rajamrit-singh <rajamritsingh1996@gmail.com>
2023-08-22 16:25:46 -0700

    added success page

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('hello world')
+let emailAddress = "amrit@gmail.com";
+
+const onFormSubmit = (event: Event) : void => {
+    event.preventDefault();
+    const emailAddressEl = document.querySelector('.form-input-email') as HTMLInputElement;
+    emailAddress = emailAddressEl.value;
+    const emailAddressSpan = document.getElementById('email-address') as HTMLSpanElement;
+    emailAddressSpan.innerHTML = emailAddress;
+
+    const main = document.getElementById('main') as HTMLDivElement;
+    main.setAttribute('display', 'none');
+    const successPage = document.getElementById('success-page') as HTMLDivElement;
+    successPage.setAttribute('display', 'grid');
+}   
